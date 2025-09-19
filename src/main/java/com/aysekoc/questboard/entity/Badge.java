d6caf344d12916eafe7bcd92eb98640e219bbbcf
aysee <ayseekoc481@gmail.com>
2025-09-19 16:25:53 +0300

    badge created

--- src/main/java/com/aysekoc/questboard/entity/Badge.java
+++ src/main/java/com/aysekoc/questboard/entity/Badge.java
@@ -1,5 +1,26 @@
 package com.aysekoc.questboard.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Badge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", unique = true, nullable = false)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="minPoints")
+    private int minPoints;
+
+
 
 }
